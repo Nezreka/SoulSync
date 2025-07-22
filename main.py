2431eba11b62c36693ded694f322fa7bda1acfa4
Broque Thomas <batmanesignal@gmail.com>
2025-07-21 19:30:30 -0700

    better

--- main.py
+++ main.py
@@ -99,8 +99,8 @@ class MainWindow(QMainWindow):
         
         # Create and add pages
         self.dashboard_page = DashboardPage()
-        self.sync_page = SyncPage(self.spotify_client, self.plex_client, self.soulseek_client)
         self.downloads_page = DownloadsPage(self.soulseek_client)
+        self.sync_page = SyncPage(self.spotify_client, self.plex_client, self.soulseek_client, self.downloads_page)
         self.artists_page = ArtistsPage()
         self.settings_page = SettingsPage()
         
